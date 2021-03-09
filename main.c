f2a13d08c8cb959c7b8a94f4f2a64d4c36f6311c
Arunan Elamaran <47310814+codeboss123@users.noreply.github.com>
2021-03-08 18:37:46 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -12,9 +12,12 @@
 #include <limits.h>
 #include <math.h>
 
+#define TOTAL_TIME     20000
+#define INTERVAL       5
+
 //------------------------------GLOBAL VARIABLES-----------------------------------------
 
-char *directory = "/Users/arunanelamaran/Desktop/STM_Project/DecisionTree/DecisionTree/";
+char *directory = "/Users/arunanelamaran/Desktop/STM_Project/DecisionTree_copy/DecisionTree/";
 
 struct Motion Stand;
 struct Motion Normal;
@@ -22,97 +25,95 @@ struct Motion Ascent;
 struct Motion Descent;
 struct Motion New;
 
+int arraylength = TOTAL_TIME/INTERVAL;
+int dataps = 1000/INTERVAL;
+
 
 //------------------------------STRUCTS-----------------------------------------
 
 struct Motion
 {
-    struct AvgInfo
-    {
-        double avg;
-        double absavg;
-        double peakavg;
-        double troughavg;
-    };
-
-    struct AvgInfo AX;
-    struct AvgInfo AY;
-    struct AvgInfo AZ;
-    struct AvgInfo GX;
-    struct AvgInfo GY;
-    struct AvgInfo GZ;
+    double AX_vals[TOTAL_TIME/INTERVAL];
+    double GY_vals[TOTAL_TIME/INTERVAL];
+    
+    double AX_avg;
+    double AX_absavg;
+    double AX_peakavg;
+    double AX_peakspefavg;
 };
 
-
-
 //------------------------------FUNCTIONS-----------------------------------
-//void normalize(char* filename);
-int analyze(char* filename, struct Motion *motionptr); //figures out basic avg
-int analyzeInfo(char* filename, int place, struct AvgInfo *axisptr, struct AvgInfo still); //figures out peak and trough avg
-
+int extract(char* filename, struct Motion *motionptr);
+void normalize(struct Motion *motionptr);
+void analyze(struct Motion *motionptr);
+void findPeaks(char* axis, struct Motion *motionptr);
 
 //------------------------------MAIN METHOD---------------------------------------
 int main(int argc, const char * argv[]) {
     
-    /*normalize("StandStill.csv");
-    normalize("Normal.csv");
-    normalize("Ascent.csv");
-    normalize("Descent.csv");
-    normalize("New.csv");*/
-    
-    Stand.AX.avg = 1; //take out later
-    
     int success;
-    success = analyze("StandStill.csv", &Stand)
-            *analyze("Normal1.csv", &Normal)
-            *analyze("Ascent1.csv", &Ascent)
-            *analyze("Descent1.csv", &Descent)
-            *analyze("New.csv", &New);
+    success = extract("StandStill.csv", &Stand)
+            *extract("Normal3.csv", &Normal)
+            *extract("Ascent3.csv", &Ascent)
+            *extract("Descent3.csv", &Descent)
+            *extract("New.csv", &New);
     
-    //if(success) { return 0; }
+    if(success) { return 0; }
     
-    printf("\nNormal Avg: %f\n", Normal.AX.absavg);
-    printf("Ascent Avg: %f\n", Ascent.AX.absavg);
-    printf("Descent: %f\n", Descent.AX.absavg);
+    normalize(&Stand);
+    normalize(&Normal);
+    normalize(&Ascent);
+    normalize(&Descent);
+    normalize(&New);
+    
+    analyze(&Stand);
+    analyze(&Normal);
+    analyze(&Ascent);
+    analyze(&Descent);
+    analyze(&New);
     
     
-    if (2==1)
-    { //check if stand still
-        
+    if (New.AX_peakavg > 1000000)
+    {
+        printf("Stand Still\n");
     }
     
-    else if(//New.AX.absavg == Normal.AX.absavg ||
-       (New.AX.absavg < Ascent.AX.absavg && New.AX.absavg < Descent.AX.absavg))
+    else if(New.AX_absavg == Normal.AX_absavg ||
+       (New.AX_absavg < Ascent.AX_absavg && New.AX_absavg < Descent.AX_absavg))
     { //could also do same thing with GZ
         printf("Normal\n");
     }
     
     else
     {
-        analyzeInfo("New.csv", 3, &(New.AZ), Stand.AZ);
-        analyzeInfo("Ascent1.csv", 3, &(Ascent.AZ), Stand.AZ);
-        analyzeInfo("Descent1.csv", 3, &(Descent.AZ), Stand.AZ);
+        findPeaks("AX", &Ascent);
+        findPeaks("AX", &Descent);
+        findPeaks("AX", &New);
+
+        double mean = (Ascent.AX_peakspefavg+Descent.AX_peakspefavg+Normal.AX_peakspefavg)/3;
+        Ascent.AX_peakspefavg /= mean;
+        Descent.AX_peakspefavg /= mean;
+        Normal.AX_peakspefavg /= mean;
         
-        printf("\nAscentPeak: %f\n", Ascent.AZ.peakavg);
-        printf("DescentPeak: %f\n", Descent.AZ.peakavg);
-        printf("AscentTrough: %f\n", Ascent.AZ.troughavg);
-        printf("DescentTrough: %f\n\n", Descent.AZ.troughavg);
+        New.AX_peakspefavg /= mean;
+    
+        mean = (Ascent.AX_peakspefavg+Descent.AX_peakspefavg+Normal.AX_peakspefavg)/3;
+        Ascent.AX_peakspefavg -= mean;
+        Descent.AX_peakspefavg -= mean;
+        Normal.AX_peakspefavg -= mean;
         
-        if(New.AZ.peakavg > Ascent.AZ.peakavg &&
-           New.AZ.troughavg > Ascent.AZ.troughavg)
-        {
-            printf("Descent\n");
-        }
+        New.AX_peakspefavg -= mean;
         
-        else if(New.AZ.peakavg < Descent.AZ.peakavg &&
-                New.AZ.troughavg < Descent.AZ.troughavg)
+        
+        if((fabs(New.AX_peakspefavg - Ascent.AX_peakspefavg)) <
+           (fabs(New.AX_peakspefavg - Descent.AX_peakspefavg)))
         {
             printf("Ascent\n");
         }
         
         else
         {
-            printf("Unclassifiable motion performed\n");
+            printf("Descent\n");
         }
     }
     
@@ -122,22 +123,10 @@ int main(int argc, const char * argv[]) {
 
 //------------------------------FUNCTION INITALIZATION-----------------------------------------
 
-/*void normalize(char* filename)
-{
-    
-}*/
-
-int analyze(char* filename, struct Motion *motionptr)
+int extract(char* filename, struct Motion *motionptr)
 {
     int count = 0;
     int place = 0;
-    
-    double AXsum = 0, absAXsum = 0;
-    double AYsum = 0, absAYsum = 0;
-    double AZsum = 0, absAZsum = 0;
-    double GXsum = 0, absGXsum = 0;
-    double GYsum = 0, absGYsum = 0;
-    double GZsum = 0, absGZsum = 0;
     
     char full_directory[500];
     strcpy(full_directory, directory);
@@ -153,7 +142,7 @@ int analyze(char* filename, struct Motion *motionptr)
     
     char line[200];
     
-    while(fgets(line, sizeof(line), file))
+    while((fgets(line, sizeof(line), file)) && (count < arraylength))
     {
         char *token;
         token = strtok(line, ",");
@@ -167,34 +156,12 @@ int analyze(char* filename, struct Motion *motionptr)
             {
                 case 1:
                     if(strcmp(filename, "StandStill.csv"))
-                        num -= Stand.AX.avg;
-                    AXsum += num;
-                    absAXsum += fabs(num);
+                        num -= 1; //num -= Stand.AX_avg;
+                    motionptr->AX_vals[count] = num;
                     break;
                     
                 case 2:
-                    AYsum += num;
-                    absAYsum += fabs(num);
-                    break;
-                
-                case 3:
-                    AZsum += num;
-                    absAZsum += fabs(num);
-                    break;
-                    
-                case 4:
-                    GXsum += num;
-                    absGXsum += fabs(num);
-                    break;
-                    
-                case 5:
-                    GYsum += num;
-                    absGYsum += fabs(num);
-                    break;
-                    
-                case 6:
-                    GZsum += num;
-                    absGZsum += fabs(num);
+                    motionptr->GY_vals[count] = num;
                     break;
             }
             
@@ -207,114 +174,76 @@ int analyze(char* filename, struct Motion *motionptr)
         place = 0;
     }
     
-    motionptr->AX.avg = AXsum/count;
-    motionptr->AY.avg = AYsum/count;
-    motionptr->AZ.avg = AZsum/count;
-    motionptr->GX.avg = GXsum/count;
-    motionptr->GY.avg = GYsum/count;
-    motionptr->GZ.avg = GZsum/count;
-    
-    motionptr->AX.absavg = absAXsum/count;
-    motionptr->AY.absavg = absAYsum/count;
-    motionptr->AZ.absavg = absAZsum/count;
-    motionptr->GX.absavg = absGXsum/count;
-    motionptr->GY.absavg = absGYsum/count;
-    motionptr->GZ.absavg = absGZsum/count;
-    
     return 1;
 }
 
-int analyzeInfo(char* filename, int place, struct AvgInfo *axisptr, struct AvgInfo still)
+void normalize(struct Motion *motionptr)
 {
-    double peaksum = 0;
-    int peakcount = 0;
-    double troughsum = 0;
-    int troughcount = 0;
-    
-    char full_directory[500];
-    strcpy(full_directory, directory);
-    strcat(full_directory, filename);
-    
-    FILE *file = fopen(full_directory, "r");
-    
-    if(file == NULL)
+    double absum = 0;
+    for(int r = 0; r < arraylength; r++)
     {
-        printf("Could not open %s. \n",filename);
-        return 0;
+        absum += fabs(motionptr->AX_vals[r]);
     }
     
-    char line[200];
+    double absavg = absum/arraylength;
     
-    while(fgets(line, sizeof(line), file))
+    for(int r = 0; r < arraylength; r++)
     {
-        char *token;
-        token = strtok(line, ",");
-        
-        int spot = 0;
-        while(token != NULL)
+        motionptr->AX_vals[r] /= absavg;
+    }
+}
+
+void analyze(struct Motion *motionptr)
+{
+    double sum = 0;
+    double absum = 0;
+    
+    for(int r = 0; r < arraylength; r++)
+    {
+        sum += motionptr->AX_vals[r];
+        absum += fabs(motionptr->AX_vals[r]);
+    }
+    
+    motionptr->AX_avg = sum/arraylength;
+    motionptr->AX_absavg = absum/arraylength;
+}
+
+void findPeaks(char* axis, struct Motion *motionptr)
+{
+    double *ptr;
+    
+    if(!strcmp(axis, "AX"))
+    {
+        ptr = motionptr->AX_vals;
+    }
+    
+    else if(!strcmp(axis, "GY"))
+    {
+        ptr = motionptr->GY_vals;
+    }
+    
+    else
+    {
+        printf("INVALID AXIS PROVIDED TO FINDPEAK FUNCTION");
+        return;
+    }
+    
+    double temppeakmax = -10000000000000;
+    double peaksum = 0;
+    
+    for(int r = 0; r < arraylength; r++)
+    {
+        if(ptr[r] > temppeakmax)
         {
-            if (spot == place)
-            {
-                char *needtoconvert;
-                double num = strtod(token, &needtoconvert); //convert token into num
-                if(num > axisptr->avg)
-                {
-                    peaksum += num;
-                    peakcount++;
-                }
-                
-                else if(num < axisptr->avg)
-                {
-                    troughsum += num;
-                    troughcount++;
-                }
-            }
-            spot++;
-            token = strtok(NULL, ",");
+            temppeakmax = ptr[r];
+        }
+        
+        if( ((r+1)%dataps) == 0)
+        {
+            peaksum += temppeakmax;
+            temppeakmax = -10000000000;
         }
     }
     
-    axisptr->peakavg = peaksum/peakcount;
-    axisptr->troughavg = troughsum/troughcount;
-    
-    return 1;
+    motionptr->AX_peakspefavg = peaksum/(TOTAL_TIME/1000);
 }
-
-
-
-
-
-/*int avg_AXnormal; //AX averages all absolute value when calcing avg
-int avg_AXascent;
-int avg_AXdescent;
-
-
-int avg_AZascent_peaks;
-int avg_AZascent_troughs;
-int avg_AZdescent_peaks;
-int avg_AZdescent_troughs;
-
-int avg_newAX;
-int avg_newAZ_peaks;
-int avg_newAZ_troughs;
-
-if(avg_newAX == avg_AXnormal ||
-   (avg_newAX < avg_AXascent && avg_newAX < avg_AXdescent))
-{ //could also do same thing with GZ
-    printf("Normal");
-}
-
-else
-{
-    if(avg_newAZ_peaks > avg_AZascent_peaks &&
-       avg_newAZ_troughs > avg_AZascent_troughs)
-    {
-        printf("Descent");
-    }
-    
-    else if(avg_newAZ_peaks < avg_AZdescent_peaks &&
-       avg_newAZ_troughs < avg_AZdescent_troughs)
-    {
-        printf("Ascent");
-    }
-}*/
